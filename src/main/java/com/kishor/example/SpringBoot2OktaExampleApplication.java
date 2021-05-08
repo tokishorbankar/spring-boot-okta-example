c4205be52cb3689531a40cd76cdead6139f7e2fa
Kishor Bankar <linkekishor@gmail.com>
2021-05-08 11:56:06 +0530

    first commit

---
+++ src/main/java/com/kishor/example/SpringBoot2OktaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kishor.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2OktaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2OktaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kishor/example/SpringBoot2OktaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kishor.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2OktaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
